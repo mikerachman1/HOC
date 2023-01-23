239dbe358a3a7b6bd4f670a215fb8197918720fd
Mike Rachman <mikerachman1@gmail.com>
2023-01-23 10:52:12 -0500

    added loading high order component

---
+++ src/withLoading.js
@@ -0,0 +1,9 @@
+import React from "react";
+function WithLoading(Component) {
+  return function WithLoadingComponent({ isLoading, ...props }) {
+    if (!isLoading) return <Component {...props} />;
+    return <p>Hold on, fetching data might take some time.</p>;
+  };
+}
+
+export default WithLoading
